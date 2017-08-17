328639e7bc13d56edfadfeb2f99503434c6462d0
Klayton <kliekkio@gmail.com>
2017-08-16 19:06:26 -0700

    almost-almost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,39 +4,79 @@
 var computerGuess = String.fromCharCode(97 + Math.round(Math.random() * 25));
 var lettersGuessed = [];
 var guesses = 0;
-var letters;
+
 var wins = 0;
 var losses = 0;
-var running;
+var running = true;
+
+
+
 
 
 document.onkeyup = function(event) {
+    if(running) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log(userGuess);
 
-var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        for (i = 0; i < guesses; i++) {
+            lettersGuessed.push(userGuess);
+            $('#lettersGuessed').append(lettersGuessed[i]);
+        }
+    }
 
-console.log(userGuess);
+    if(userGuess === computerGuess) {
+        wins++;
+        $('#numWins').html(wins);
 
-for (i = 0; i < guesses; i++) {
+     } else if (userGuess !== computerGuess) {
+        guesses++;
+        $('#guesses').html(guesses);
+    }
 
+    if(guesses === 10) {
+        losses++;
+        $('#losses').html(losses);
+        gameOver();
+    }
 }
 
-function running()
-
-lettersGuessed.push(letters);
-
-if (userGuess === computerGuess) {
-	wins++;
-	$("#wins").html(wins);
- } else if (userGuess !== computerGuess) {
-	guesses++;
-	$("#guesses").html(guesses);
-}
-
-if (guesses === 10) {
-	losses++;
-	$("#losses").html(losses);
+function gameOver(){
+    running = false;
 }
 
 
-	};
+// document.onkeyup = function(event) {
+// 	if(running) {
+
+
+// var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+// console.log(userGuess);
+
+// for (i = 0; i < guesses; i++) {
+
+
+// }
+
+// lettersGuessed.push(letters);
+
+// if (userGuess === computerGuess) {
+// 	wins++;
+// 	$("#wins").html(wins);
+//  } else if (userGuess !== computerGuess) {
+// 	guesses++;
+// 	$("#guesses").html(guesses);
+// }
+
+// if (guesses === 10) {
+// 	losses++;
+// 	$("#losses").html(losses);
+// 	gameOver();
+
+// }
+// }
+
+
+
+// 	};
 
